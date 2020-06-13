226305d5aba7259d251225e2422006a753e52829
Ivan Arredondo <a16310034@ceti.mx>
2020-06-12 20:09:20 -0500

    se removio la carpeta que sobraba

--- 3P/01-RMI_Paso_de_mensaje/Cliente/MiInterfazRemota.java
+++
@@ -1,5 +0,0 @@
-//16310034 - Ivan Emmanuel Arredondo Martinez
-public interface MiInterfazRemota extends java.rmi.Remote{
-    public void miMetodo1(String texto) throws java.rmi.RemoteException;
-    public int miMetodo2() throws java.rmi.RemoteException;
-}
